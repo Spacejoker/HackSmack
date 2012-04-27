f3d6e5400bc77ecebaaff2425ef219dcd9a2aa52
Jonas <engblom.jonas@gmail.com>
2012-04-27 10:49:12 +0200

    Testar att comitta igen
    Signed-off-by: Jonas <engblom.jonas@gmail.com>

--- src/com/inda/hacksmack/model/Enemy.java
+++ src/com/inda/hacksmack/model/Enemy.java
@@ -10,6 +10,7 @@ public class Enemy extends Entity {
 	
 	private double defaultspeed;
 	private Animation walkAnimation;
+	boolean playerNoticed = false;
 	
 	public Enemy(){
 		passable = false;
@@ -26,28 +27,39 @@ public class Enemy extends Entity {
 	 */
 
 	public void updateDirection(Player player) {
-		float py = player.getPosition().y;
-		float px = player.getPosition().x;
-		float ey = position.y;
-		float ex = position.x;
 		
-		direction.x = px - ex;
-		direction.y = py - ey;
 		
-		direction.normalise();
-		
-		if(direction.x*direction.x + direction.y * direction.y > 0.1){
-			direction.normalise();
-		} else {
-			direction.x = 0;
-			direction.y = 0;
-		}
-		
-		if(Math.sqrt(Math.pow(position.x - player.getPosition().x, 2) + Math.pow(position.y - player.getPosition().y, 2)) < player.getRadius() + radius){
+		if(playerNoticed){
+			float py = player.getPosition().y;
+			float px = player.getPosition().x;
+			float ey = position.y;
+			float ex = position.x;
 			
-			speed = 0;
+			direction.x = px - ex;
+			direction.y = py - ey;
+			
+			direction.normalise();
+			
+			if(direction.x*direction.x + direction.y * direction.y > 0.1){
+				direction.normalise();
+			} else {
+				direction.x = 0;
+				direction.y = 0;
+			}
+			
+			if(Math.sqrt(Math.pow(position.x - player.getPosition().x, 2) + Math.pow(position.y - player.getPosition().y, 2)) < player.getRadius() + radius){
+				
+				speed = 0;
+			} else {
+				speed = defaultspeed;
+			}
 		} else {
-			speed = defaultspeed;
+			speed = 0;
+			
+			if(player.position.distance(position) < 300){
+				playerNoticed = true;
+				System.out.print("Hittad!");
+			}
 		}
 	}
 	

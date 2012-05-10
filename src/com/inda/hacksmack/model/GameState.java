6dc4a79282a67d7c46e9b7799ecb9516b802945b
Jens Staahl <stahl.jens@gmail.com>
2012-05-10 10:26:25 +0200

    changed order of maps

--- src/com/inda/hacksmack/model/GameState.java
+++ src/com/inda/hacksmack/model/GameState.java
@@ -36,7 +36,7 @@ public class GameState {
 	private HealthBar healthBar = new HealthBar();
 	private HeatAndBatteryBar heatAndBatteryBar = new HeatAndBatteryBar();
 	int mapNr = 0;
-	String[] mapNames = new String[]{"level_4", "level_1", "level_2", "level_3", "level_4"};
+	String[] mapNames = new String[]{"level_0", "level_1", "level_2", "level_3", "level_4"};
 
 	static int gamestates = 0;
 	int gamestate = gamestates;
